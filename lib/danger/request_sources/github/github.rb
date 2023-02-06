f2082c31cf74fe5dd45de91f1706c3d3a422abf3
ciihla <jan.uhlar@productboard.com>
2023-02-06 16:41:30 +0100

    debug commit_not_exists

--- lib/danger/request_sources/github/github.rb
+++ lib/danger/request_sources/github/github.rb
@@ -104,6 +104,11 @@ module Danger
         head_commit = self.pr_json["head"]["sha"]
 
         # Next, we want to ensure that we have a version of the current branch at a known location
+        puts "Debug setup_danger_branches:"
+        puts "base_branch: #{base_branch}"
+        puts "base_commit: #{base_commit}"
+        puts "head_branch: #{head_branch}"
+        puts "head_commit: #{head_commit}"
         scm.ensure_commitish_exists_on_branch! base_branch, base_commit
         self.scm.exec "branch #{EnvironmentManager.danger_base_branch} #{base_commit}"
 
--- lib/danger/scm_source/git_repo.rb
+++ lib/danger/scm_source/git_repo.rb
@@ -123,7 +123,11 @@ module Danger
     end
 
     def commit_not_exists?(sha1)
-      exec("rev-parse --quiet --verify #{sha1}^{commit}").empty?
+      commit = exec("rev-parse --quiet --verify #{sha1}^{commit}")
+      puts "Debug:"
+      puts "rev-parse --quiet --verify #{sha1}^{commit}"
+      puts "Commit #{commit}"
+      commit.empty?
     end
 
     def find_merge_base(repo, from, to)
